6bbae60f987a264db4376314194f2214071fc00c
tebyboho <estebanbohorquez86@gmail.com>
2025-04-15 20:17:42 -0300

    Ejercicio 002.py

--- 05 - Funciones/001.py
+++ 05 - Funciones/001.py
@@ -1,4 +1,8 @@
-
+'''
+1. Crear una función llamada imprimir_hola_mundo que imprima por
+pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
+programa principal.
+'''
 
 def saludo():
     print("Hola mundo!!")
