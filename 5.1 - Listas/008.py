5dec2d1eeec836a6fb1f66fbc8c1eac4dcc0d97b
tebyboho <estebanbohorquez86@gmail.com>
2025-04-23 09:26:29 -0300

    Ejercicio 008.py

---
+++ 5.1 - Listas/008.py
@@ -0,0 +1,12 @@
+'''
+8) Crear una lista vacía llamada "dobles" y agregar el doble de 5, 10 y 15 usando append
+directamente. Imprimir la lista resultante por pantalla.
+'''
+
+
+dobles = []
+
+for e in [5, 10, 15]:
+    dobles.append(e * 2)
+
+print(dobles)
