000e28bbc5f58a43512d2a21b5fd4da1b345af49
tebyboho <estebanbohorquez86@gmail.com>
2025-04-23 09:22:15 -0300

    Ejercicio 006.py

---
+++ 5.1 - Listas/006.py
@@ -0,0 +1,8 @@
+'''
+6) Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por
+pantalla los dos primeros.
+'''
+
+numeros = [num for num in range(10, 31, 5)]
+
+print(numeros[0:2])
