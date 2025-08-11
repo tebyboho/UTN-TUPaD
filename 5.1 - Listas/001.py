68298c67bbd7140d02b3386059dabd4a50923911
tebyboho <estebanbohorquez86@gmail.com>
2025-08-11 20:20:40 -0300

    Delete 5.1 - Listas directory

--- 5.1 - Listas/001.py
+++
@@ -1,8 +0,0 @@
-'''
-1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función
-range.
-'''
-
-lista = [i for i in range(4,101,4)] # Se usó list comprehension 
-
-print(lista)
--- 5.1 - Listas/006.py
+++
@@ -1,8 +0,0 @@
-'''
-6) Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por
-pantalla los dos primeros.
-'''
-
-numeros = [num for num in range(10, 31, 5)]
-
-print(numeros[0:2])
