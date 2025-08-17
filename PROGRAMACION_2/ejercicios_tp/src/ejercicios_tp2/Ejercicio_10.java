577eee19dfbdd3d8d11ed6f0ac9779b45869490f
tebyboho <estebanbohorquez86@gmail.com>
2025-08-17 19:52:20 -0300

    Ejercicios TP 2 Semana 2

---
+++ PROGRAMACION_2/ejercicios_tp/src/ejercicios_tp2/Ejercicio_10.java
@@ -0,0 +1,25 @@
+package ejercicios_tp2;
+import java.util.Scanner;
+
+public class Ejercicio_10 {
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el stock actual del producto: ");
+        int stockActual = scanner.nextInt();
+
+        System.out.print("Ingrese la cantidad vendida: ");
+        int cantidadVendida = scanner.nextInt();
+
+        System.out.print("Ingrese la cantidad recibida: ");
+        int cantidadRecibida = scanner.nextInt();
+
+        int nuevoStock = actualizarStock(stockActual, cantidadVendida, cantidadRecibida);
+
+        System.out.println("El nuevo stock del producto es: " + nuevoStock);
+    }
+
+    public static int actualizarStock(int stockActual, int cantidadVendida, int cantidadRecibida) {
+        return stockActual - cantidadVendida + cantidadRecibida;
+    }
+}
