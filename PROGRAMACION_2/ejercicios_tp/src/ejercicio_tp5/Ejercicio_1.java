e8b3eb8c0d349a18f286de8f1362c67c05635e41
tebyboho <estebanbohorquez86@gmail.com>
2025-09-12 21:02:08 -0300

    Ejercicio TP Semana 5 - POO

---
+++ PROGRAMACION_2/ejercicios_tp/src/ejercicio_tp5/Ejercicio_1.java
@@ -0,0 +1,26 @@
+package ejercicio_tp5;
+
+public class Ejercicio_1 {
+    public static void main(String[] args){
+        
+        // constructor que solo recibe nombre y puesto
+        Empleado e1 = new Empleado("Ana Pérez", "Developer");
+        
+        // constructor que recibe todos los parametros
+        Empleado e2 = new Empleado(10, "Juan Gómez", "Analista", 45000.0);
+        
+        // Llamando metodos de instancias
+        System.out.println(e1);
+        System.out.println(e2);
+
+        e1.actualizarSalario(10);    // +10%
+        e2.actualizarSalario(2000);  // +2000 fijo
+
+        System.out.println("Después de incrementos:");
+        System.out.println(e1);
+        System.out.println(e2);
+        
+        // Llamando metodo de la Clase Empleado
+        System.out.println("Total empleados: " + Empleado.mostrarTotalEmpleados());
+    }
+}
